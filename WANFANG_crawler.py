15d6360a2bc5dc22d7af14ee0c8ebfe29f5d3aca
Jyjays <jiang13475384640@outlook.com>
2024-10-16 23:03:53 +0800

    feat: Get category and item data from WANFANG

--- WANFANG_crawler.py
+++ WANFANG_crawler.py
@@ -3,6 +3,12 @@ import utils
 # 初始化WebDriver
 from utils import driver
 
+paper_list = '/html/body/div[5]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div'
+
+year_path = '/html/body/div[5]/div/div[3]/div[2]/div[2]/div[2]/div[1]/div/div[2]/div[3]/div[1]/i'
+year_list = '/html/body/div[5]/div/div[3]/div[2]/div[2]/div[2]/div[1]/div/div[2]/div[3]/div[2]/div'
+
+
 
 # 以下是万方网站特定函数
 def WANFANG_set_class1_author():
@@ -52,15 +58,36 @@ WANFANG_set_time_range(2014,0)
 #点击搜索
 WANFANG_click_search()
 #/html/body/div[5]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div
+
+# 根据年份获取文献数量
+utils.click_by_xpath(year_path)
+year_outer_text = utils.parse_outer_text(year_list)
+
+
 # 获取当前页的文章数量
-num = utils.parse_element_count('/html/body/div[5]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div')
-for i in range(1,num+1):
+category_data = [['期刊论文',0],['会议论文',0]]
+item_data = []
+num = utils.parse_element_count(paper_list)
+for i in range(1,num):
     category = utils.parse_outer_text(f'/html/body/div[5]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div/div[{i}]/div/div[2]/span[1]')
     if category == '[期刊论文]':
-        print('期刊论文')
+        category_data[0][1] += 1
     elif category == '[会议论文]':
-        print('会议论文')
-        
+        category_data[1][1] += 1
+    item_count = utils.parse_element_count(f'/html/body/div[5]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div/div[{i}]/div/div[4]')
+    if item_count == 0 | item_count == ():
+        continue
+    for j in range(1,item_count+1):
+        item = utils.parse_outer_text(f'/html/body/div[5]/div/div[3]/div[2]/div[2]/div[2]/div[2]/div/div/div[2]/div[1]/div/div[{i}]/div/div[4]/span[{j}]')
+        item_data.append(item)
+
+item_map = []
+for item in item_data:
+    if item in item_map:
+        item_map[item_map.index(item)][1] += 1
+    else:
+        item_map.append((item,1))
+print(item_map)
 
 # 关闭浏览器
 driver.quit()
